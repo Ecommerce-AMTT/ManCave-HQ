e19d0902c8f82ae3ab41d1fa67476869e0759159
Anthony Poku <tonypoku@gmail.com>
2022-09-06 09:34:41 -0500

    minor cleanup for project setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://mongouser:SYU4vFKFyPugtmo4@cluster0.41g20xy.mongodb.net/googlebooks" ||
-    "mongodb://127.0.0.1:27017/googlebooks",
+    // "mongodb+srv://mongouser:SYU4vFKFyPugtmo4@cluster0.41g20xy.mongodb.net/googlebooks" ||
+    "mongodb://127.0.0.1:27017/homeoffice",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
